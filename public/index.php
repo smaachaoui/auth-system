6a93a265976887544aa8389f996d244c4a52a314
seif <smaachaoui@outlook.com>
2026-01-17 17:41:22 +0100

    First commit

---
+++ public/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Module de connexion - Accueil</title>
+    </head>
+
+    <body>
+        <header>
+            <h1>Hello world !</h1>
+        </header>
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+        
+    </body>
+</html>
